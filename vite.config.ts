3f691e39ac359529e8576dcfc39b9c8825aa14b2
Vegeta0807 <104052637+Vegeta0807@users.noreply.github.com>
2025-09-29 09:16:51 +0000

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/aditya-software-portfolio/', // <-- must match repo name
+  base: '/aditya-software-portfolio/', 
   plugins: [react()],
   resolve: {
     alias: {
